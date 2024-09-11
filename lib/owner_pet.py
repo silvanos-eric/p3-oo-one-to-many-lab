a2e20e4ce5a511ac7dda1fb12de0727ae446f951
Silvanos Eric <ericoderito@gmail.com>
2024-09-11 19:46:26 +0300

    feat(lib): implement Pet and Owner classes with methods
    - Added PET_TYPES and all attributes to Pet class.
    - Implemented __init__ method for Pet class to initialize pet instances and validate pet types.
    - Implemented Owner class with __init__, pets, add_pet, and get_sorted_pets methods.
    - Ensured pets are associated with owners and can be sorted by name.

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,29 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.owner = owner
+        if pet_type not in self.PET_TYPES:
+            raise TypeError(f"Pet type must be one of {self.PET_TYPES}")
+        else:
+            self.pet_type = pet_type
+        Pet.all.append(self)
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("Pet must be an instace of Pet class")
+        else:
+            pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
